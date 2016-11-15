2753a853ecc6b0355463b97a183fd11ecc690863
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-15 11:54:38 -1000

    creates Node class

--- graphGenerator.js
+++ graphGenerator.js
@@ -1,8 +1,18 @@
 // Implement a graph generator that represents a set of objects where some pairs of objects are connected by links.
 
 const Node = function(name, value) {
-
+  this.name = name;
+  this.value = value;
+  this.neighbors = [];
 }
 
+Node.prototype.addNeighbors = function(newNeighbors) {
+  this.neighbors = this.neighbors.concat(newNeighbors);
+  return this.neighbors;
+}
+
+Node.prototype.toString = function() {
+  return this.name;
+}
 
 module.exports = Node;
