1b4d7ff37d0b2a665bcb60f79e3adaeeb29c6919
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-15 12:47:53 -1000

    passes breadth first tests

--- breadthFirstSearch.js
+++ breadthFirstSearch.js
@@ -2,10 +2,30 @@
 
 const Node = require('./graphGenerator');
 
+const haveAllBeenVisited = (element, index, array) => {
+  return element.visited === true;
+}
+
 const BFS = (start) => {
   let queue = [start];
+  let visitedNodes = [start];
 
-  return queue.toString();
+  queue[0].visited = true;
+
+  while(queue.length > 0) {
+    //if the first node in the queue has neighbors, add them all to the stack in order
+    if(queue[0].neighbors.length > 0) {
+      queue[0].neighbors.forEach((node) => {
+        node.visited = true;
+        queue.push(node);
+        visitedNodes.push(node);
+      })
+    }
+    if(queue.every(haveAllBeenVisited)) {
+      queue.shift();
+    }
+  }
+  return visitedNodes.toString();
 }
 
 module.exports = BFS;
